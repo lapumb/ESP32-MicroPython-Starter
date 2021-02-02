1db6a5f5e96cdef36e3b4b0c27890b423e47dbd3
Nick Miller <nick.miller@spindance.com>
2021-02-02 08:49:48 -0800

    flash: increase baud rate to 460800

--- MicroPython/flash_micropython.sh
+++ MicroPython/flash_micropython.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-esptool.py --chip esp32 --port $ESPPORT write_flash -z 0x1000 $MICROPYTHON_ESP_FIRMWARE
+esptool.py --chip esp32 --port $ESPPORT --baud 460800 write_flash -z 0x1000 $MICROPYTHON_ESP_FIRMWARE

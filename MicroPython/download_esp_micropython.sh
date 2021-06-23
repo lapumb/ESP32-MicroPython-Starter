b5abffa6ab2835411a2d2043884f45256419ab5a
Blake Lapum <blake.lapum@spindance.com>
2021-06-23 13:32:04 -0400

    update gitignore, small cleanup

--- MicroPython/download_esp_micropython.sh
+++ MicroPython/download_esp_micropython.sh
@@ -5,7 +5,7 @@ cd $REPO_ROOT/MicroPython
 # Remove the binary if it already exists
 rm "$MICROPYTHON_ESP_FIRMWARE"
 
-echo "Installing MicroPython ESP-IDF v3"
+echo "Installing MicroPython: $MICROPYTHON_ESP_FIRMWARE"
 wget https://micropython.org/resources/firmware/$MICROPYTHON_ESP_FIRMWARE
 
 cd -
--- MicroPython/flash_micropython.sh
+++ MicroPython/flash_micropython.sh
@@ -3,5 +3,8 @@
 set -e
 
 cd $REPO_ROOT/MicroPython
+
+echo "Flashing MicroPython: $MICROPYTHON_ESP_FIRMWARE"
 esptool.py --chip esp32 --port $ESPPORT --baud 460800 write_flash -z 0x1000 $MICROPYTHON_ESP_FIRMWARE
+
 cd -
